86b70951bacba3b7f5415d5fa385bad8b42bff8d
Pranit <pranit@sphinx-solution.com>
2024-11-28 09:50:07 +0530

    Theme Problem solve

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-const String BASE_URL = "http://192.168.1.200:5000/api";
+const String BASE_URL = "http://192.168.2.77:5000/api";
 const Map<String, dynamic> DEFAULT_HEADERS = {
   "Content-Type": "application/json"
 };
